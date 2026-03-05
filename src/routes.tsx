f6fc34c2a9dbbbfe814d8c9243744a2b1294e069
rim abboud <rim.abboud1@met.usj.edu.lb>
2026-03-05 19:49:02 +0200

    added gallery page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
+import AdminPage from "./pages/AdminPage";
+import GalleryPage from "./pages/GalleryPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/login", element: <LoginPage /> },
   { path: "/signup", element: <SignupPage /> },
+  { path: "/gallery", element: <GalleryPage /> },
+  { path: "/Admin", element: <AdminPage /> },
+  { path: "/admin", element: <AdminPage /> },
 ]);
